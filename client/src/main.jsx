cfad8ba01a95a552c4ff1454c8983e591c723a3a
olegmishutin <omisutin646@gmail.com>
2024-09-03 15:52:33 +0300

    added base for client

---
+++ client/src/main.jsx
@@ -0,0 +1,22 @@
+import './main.css'
+
+import {createRoot} from 'react-dom/client'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+
+import Login from "./pages/Login/login.jsx";
+import Registration from "./pages/Registration/registration.jsx";
+
+const router = createBrowserRouter([
+    {
+        path: 'registration/',
+        element: <Registration/>
+    },
+    {
+        path: 'login/',
+        element: <Login/>
+    }
+])
+
+createRoot(document.getElementById('root')).render(
+    <RouterProvider router={router}/>
+)
