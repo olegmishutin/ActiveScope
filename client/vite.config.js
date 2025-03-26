bc8747f7bdc18d8d13c9a6f9ed55a5e3b83984f1
olegmishutin <omisutin646@gmail.com>
2025-03-26 21:39:28 +0300

    fixed

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,7 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
-const DOCKERIZED = false
+const DOCKERIZED = true
 
 // https://vitejs.dev/config/
 export default defineConfig({
